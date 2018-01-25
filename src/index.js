da43235043cccd526f4eba53b8566121180c73bf
jacekk <github.com@jacek-k.net>
2018-01-25 01:04:06 +0100

    added some endpoints; renamed project;

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 const Hapi = require('hapi')
+const Joi = require('joi')
+
+const contacts = []
 
 const server = Hapi.server({
     host: 'localhost',
     port: 8000,
+    routes: {
+        validate: {
+            // https://github.com/hapijs/hapi/issues/3706
+            failAction: async (request, h, err) => {
+                if (process.env.NODE_ENV === 'production') {
+                    // In prod, log a limited error message and throw the default Bad Request error.
+                    console.error('ValidationError:', err.message) // Better to use an actual logger here.
+                    throw Boom.badRequest(`Invalid request payload input`)
+                } else {
+                    // During development, log and respond with the full error.
+                    console.error(err)
+                    throw err
+                }
+            },
+        },
+    },
 })
 
 const getRandomString = () =>
@@ -23,6 +42,45 @@ server.route({
 
         return `Hello anonymous '${getRandomString()}' :)`
     },
+    config: {
+        validate: {
+            params: {
+                name: Joi.string()
+                    .alphanum()
+                    .min(3)
+                    .max(30)
+                    .required(),
+            },
+        },
+    },
+})
+
+server.route({
+    method: 'GET',
+    path: '/contacts',
+    handler(request, h) {
+        return h.response({ contacts }).code(201)
+    },
+})
+
+server.route({
+    method: 'POST',
+    path: '/contacts',
+    config: {
+        validate: {
+            payload: Joi.object({
+                contact: Joi.object({
+                    name: Joi.string().required(),
+                    surname: Joi.string().required(),
+                }).required(),
+            }),
+        },
+    },
+    handler(request, h) {
+        const contact = request.payload.contact
+        contacts.push(contact)
+        return { contact }
+    },
 })
 
 server.route({
@@ -30,6 +88,15 @@ server.route({
     path: '/search',
     handler(request, h) {
         return request.query
+    },
+    config: {
+        validate: {
+            query: {
+                text: Joi.string().required(),
+                pageNumber: Joi.number().default(1),
+                lang: Joi.only(['pl', 'gb']).default('pl'),
+            },
+        },
     },
 })
 
